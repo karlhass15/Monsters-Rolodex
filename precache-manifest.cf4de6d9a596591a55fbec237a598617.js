943659c4773ee8a684d04786fb83d3330a3afabb
Karl Hass <karl.hass15@gmail.com>
2019-10-09 14:09:02 -0500

    Updates

---
+++ precache-manifest.cf4de6d9a596591a55fbec237a598617.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de9b5b3333ce913862ed5df25a4aa87",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "40e80ee99ebf6ef0d8e0",
+    "url": "/Monsters-Rolodex/static/css/main.00ad1177.chunk.css"
+  },
+  {
+    "revision": "e1d8890a54874a546b49",
+    "url": "/Monsters-Rolodex/static/js/2.a7fab877.chunk.js"
+  },
+  {
+    "revision": "40e80ee99ebf6ef0d8e0",
+    "url": "/Monsters-Rolodex/static/js/main.3af403b4.chunk.js"
+  },
+  {
+    "revision": "11e2dea3cac39d985a42",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.3ef3660b.js"
+  }
+]);
